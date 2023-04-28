d568f4073e9b9e4052b2ba358308c5941e05b465
viljawallin <wavi22hy@student.ju.se>
2023-04-28 19:16:13 +0200

    more levels

--- .github/levels.js
+++ .github/levels.js
@@ -7,13 +7,75 @@ function setup() {
   background(253, 212, 238);
 }
 
-function scenery() {
+// function level1() {
+//   push();
+//   noStroke();
+//   fill(255, 245, 230);
+//   rect(0, 0, 900, 600);
+
+//   fill(252, 193, 166);
+//   rect(0, 490, 900);
+//   rect(0, 0, 900, 50);
+//   pop();
+
+//   push();
+//   textSize(35);
+//   fill(255, 245, 230);
+//   textFont("Comic Sans MS");
+//   text("LEVEL 1", 360, 40);
+//   pop();
+
+//   push();
+//   stroke(170, 170, 170);
+//   strokeWeight(6);
+//   square(20, 160, 230, 30);
+//   pop();
+
+//   push();
+//   noStroke();
+//   fill(180, 130, 100);
+//   rect(125, 393, 20, 97);
+//   pop();
+// }
+
+// function level3() {
+//   push();
+//   noStroke();
+//   fill(255, 225, 230);
+//   rect(0, 0, 900, 600);
+
+//   fill(255, 100, 120);
+//   rect(0, 490, 900);
+//   rect(0, 0, 900, 50);
+//   pop();
+
+//   push();
+//   textSize(35);
+//   fill(255, 225, 230);
+//   textFont("Comic Sans MS");
+//   text("LEVEL 3", 360, 40);
+//   pop();
+
+//   push();
+//   stroke(170, 170, 170);
+//   strokeWeight(6);
+//   square(20, 160, 230, 30);
+//   pop();
+
+//   push();
+//   noStroke();
+//   fill(180, 130, 100);
+//   rect(125, 393, 20, 97);
+//   pop();
+// }
+
+function level5() {
   push();
   noStroke();
   fill(255, 245, 230);
   rect(0, 0, 900, 600);
 
-  fill(252, 193, 166);
+  fill(255, 130, 180);
   rect(0, 490, 900);
   rect(0, 0, 900, 50);
   pop();
@@ -22,7 +84,7 @@ function scenery() {
   textSize(35);
   fill(255, 245, 230);
   textFont("Comic Sans MS");
-  text("LEVEL 1", 360, 40);
+  text("LEVEL 5", 360, 40);
   pop();
 
   push();
@@ -30,8 +92,16 @@ function scenery() {
   strokeWeight(6);
   square(20, 160, 230, 30);
   pop();
+
+  push();
+  noStroke();
+  fill(180, 130, 100);
+  rect(125, 393, 20, 97);
+  pop();
 }
 
 function draw() {
-  scenery();
+  //   level1();
+  //   level3();
+  level5();
 }
