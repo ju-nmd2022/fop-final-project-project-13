5cd01fd49b048bf3921f6918ead51057f3ab84bb
miastu <mia.studenac@gmail.com>
2023-05-01 11:59:52 +0200

    levels

--- .github/levels.js
+++ .github/levels.js
@@ -100,8 +100,104 @@ function level5() {
   pop();
 }
 
+function level2() {
+  push();
+  noStroke();
+  fill(244, 220, 220);
+  rect(0, 0, 900, 600);
+
+  fill(129, 129, 129);
+  rect(0, 490, 900);
+  rect(0, 0, 900, 50);
+  pop();
+
+  push();
+  textSize(35);
+  fill(255, 245, 230);
+  textFont("Comic Sans MS");
+  text("LEVEL 2", 360, 40);
+  pop();
+
+  push();
+  stroke(170, 170, 170);
+  strokeWeight(6);
+  square(20, 160, 230, 30);
+  pop();
+
+  push();
+  noStroke();
+  fill(180, 130, 100);
+  rect(125, 393, 20, 97);
+  pop();
+}
+
+function level4() {
+  push();
+  noStroke();
+  fill(243, 242, 242);
+  rect(0, 0, 900, 600);
+
+  fill(181, 209, 209);
+  rect(0, 490, 900);
+  rect(0, 0, 900, 50);
+  pop();
+
+  push();
+  textSize(35);
+  fill(255, 245, 230);
+  textFont("Comic Sans MS");
+  text("LEVEL 4", 360, 40);
+  pop();
+
+  push();
+  stroke(170, 170, 170);
+  strokeWeight(6);
+  square(20, 160, 230, 30);
+  pop();
+
+  push();
+  noStroke();
+  fill(180, 130, 100);
+  rect(125, 393, 20, 97);
+  pop();
+}
+
+function level6() {
+  push();
+  noStroke();
+  fill(249, 168, 168);
+  rect(0, 0, 900, 600);
+
+  fill(137, 201, 162);
+  rect(0, 490, 900);
+  rect(0, 0, 900, 50);
+  pop();
+
+  push();
+  textSize(35);
+  fill(255, 245, 230);
+  textFont("Comic Sans MS");
+  text("LEVEL 4", 360, 40);
+  pop();
+
+  push();
+  stroke(170, 170, 170);
+  strokeWeight(6);
+  square(20, 160, 230, 30);
+  pop();
+
+  push();
+  noStroke();
+  fill(180, 130, 100);
+  rect(125, 393, 20, 97);
+  pop();
+}
+
 function draw() {
   //   level1();
   //   level3();
-  level5();
+  //level5();
+  //level2();
+  //level4();
+  level6();
 }
