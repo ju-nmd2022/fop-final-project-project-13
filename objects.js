4c18f700508b97c6fad592beccfd8e8e354bd9ed
miastu <mia.studenac@gmail.com>
2023-05-18 18:05:33 +0200

    mushroom and watermelon

--- objects.js
+++ objects.js
@@ -138,19 +138,173 @@ function IceCream(x, y, s) {
   };
 }
 
+//mushroom
+
+function Mushroom(x, y, s) {
+  this.x = x + 70;
+  this.y = y;
+  this.s = s;
+
+  this.drawSquare1 = function () {
+    push();
+    noStroke();
+    fill(240, 192, 168);
+    square(this.x + 200 * this.s, this.y + 80 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare2 = function () {
+    push();
+    noStroke();
+    fill(255, 30, 30);
+    square(this.x + 200 * this.s, this.y + 30 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare3 = function () {
+    push();
+    noStroke();
+    fill(255, 30, 30);
+    square(this.x + 250 * this.s, this.y + 30 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare4 = function () {
+    push();
+    noStroke();
+    fill(255, 30, 30);
+    square(this.x + 175 * this.s, this.y - 20 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare5 = function () {
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    square(this.x + 225 * this.s, this.y - 20 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare6 = function () {
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    square(this.x + 150 * this.s, this.y + 30 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.display = function () {
+    this.drawSquare1();
+    this.drawSquare2();
+    this.drawSquare3();
+    this.drawSquare4();
+    this.drawSquare5();
+    this.drawSquare6();
+  };
+}
+
+//watermelon
+
+function Watermelon(x, y, s) {
+  this.x = x + 200;
+  this.y = y + 260;
+  this.s = s;
+
+  this.drawSquare1 = function () {
+    push();
+    fill(95, 202, 125);
+    noStroke();
+    square(this.x + 10 * this.s, this.y + 60 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare2 = function () {
+    push();
+    fill(95, 202, 125);
+    noStroke();
+    square(this.x + 110 * this.s, this.y + 60 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare3 = function () {
+    push();
+    fill(40, 167, 75);
+    noStroke();
+    square(this.x + 60 * this.s, this.y + 60 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare4 = function () {
+    push();
+    fill(217, 58, 58);
+    noStroke();
+    square(this.x + 10 * this.s, this.y + 10 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare5 = function () {
+    push();
+    fill(217, 58, 58);
+    noStroke();
+    square(this.x + 85 * this.s, this.y - 40 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare6 = function () {
+    push();
+    fill(217, 58, 58);
+    noStroke();
+    square(this.x + 110 * this.s, this.y + 10 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare7 = function () {
+    push();
+    fill(233, 71, 71);
+    noStroke();
+    square(this.x + 60 * this.s, this.y + 10 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare8 = function () {
+    push();
+    fill(233, 71, 71);
+    noStroke();
+    square(this.x + 35 * this.s, this.y - 40 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.drawSquare9 = function () {
+    push();
+    fill(233, 71, 71);
+    noStroke();
+    square(this.x + 60 * this.s, this.y - 90 * this.s, this.s * 50);
+    pop();
+  };
+
+  this.display = function () {
+    this.drawSquare1();
+    this.drawSquare2();
+    this.drawSquare3();
+    this.drawSquare4();
+    this.drawSquare5();
+    this.drawSquare6();
+    this.drawSquare7();
+    this.drawSquare8();
+    this.drawSquare9();
+  };
+}
+
 function draw() {
   background(253, 212, 238);
 
+  var myWatermelon = new Watermelon(100, 100, 1);
+  var myMushroom = new Mushroom(100, 100, 1);
   var myIceCream = new IceCream(100, 100, 1);
-
-  // Create a Duck object and display it
   var duck = new Duck(-200, 170, 1);
 
-  // Draw individual squares selectively
-  duck.drawSquare1();
-  duck.drawSquare3();
-
-  // Display the entire duck
   duck.display();
   myIceCream.display();
+  myMushroom.display();
+  myWatermelon.display();
 }
