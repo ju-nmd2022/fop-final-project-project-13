e4eaa572378a03dbc60c57ef26c7d97905f51fda
viljawallin <wavi22hy@student.ju.se>
2023-05-19 12:27:23 +0200

    trying to make a collide between squares and kirby

--- testingstuff.js
+++ testingstuff.js
@@ -1,63 +1,47 @@
-let particles = [];
 let x = 100;
 let y = 100;
 let s = 1;
 let moveLeft = false;
 let moveRight = false;
-let score = 0;
-
-const particleSize = 70; // Set the size of the particles
-
-function createParticle() {
-  const x = Math.random() * width;
-  const y = -particleSize; // Set initial y position to a negative value
-  const v = 0.5 + Math.random() * 3.5; // Adjust the velocity range
-  const shape = Math.random() < 0.5 ? "square" : "circle";
-  const color = shape === "square" ? "green" : "red";
-  return { x: x, y: y, velocity: v, shape: shape, color: color };
-}
-
-function drawParticle(particle) {
-  push();
-  translate(particle.x, particle.y);
-  noStroke();
-
-  if (particle.shape === "square") {
-    fill(190, 255, 180);
-    rectMode(CENTER);
-    rect(0, 0, particleSize, particleSize);
-  } else if (particle.shape === "circle") {
-    fill(240, 100, 120);
-    ellipse(0, 0, particleSize, particleSize);
-  }
-
-  pop();
-}
-
-function updateParticle(particle) {
-  particle.y = particle.y + particle.velocity;
-
-  // Wrap the particle when it reaches the bottom of the canvas
-  if (particle.y > height + particleSize) {
-    particle.y = -particleSize;
-  }
-
-  if (checkCollision(particle)) {
-    // Particle collided with Kirby
-    score++;
-    particle.y = -particleSize; // Reset the particle's position
-  }
-}
+let particles = [];
+const particleSize = 70;
 
 function setup() {
   createCanvas(900, 600);
   background(253, 212, 238);
-
-  for (let i = 0; i < 4; i++) {
-    const particle = createParticle();
-    particles.push(particle);
-  }
 }
+
+function level1() {
+  push();
+  noStroke();
+  fill(255, 245, 230);
+  rect(0, 0, 900, 600);
+
+  fill(252, 193, 166);
+  rect(0, 490, 900);
+  rect(0, 0, 900, 50);
+  pop();
+
+  push();
+  textSize(35);
+  fill(255, 245, 230);
+  textFont("Comic Sans MS");
+  text("LEVEL 1", 360, 40);
+  pop();
+
+  push();
+  stroke(170, 170, 170);
+  strokeWeight(6);
+  square(20, 160, 230, 30);
+  pop();
+
+  push();
+  noStroke();
+  fill(180, 130, 100);
+  rect(125, 393, 20, 97);
+  pop();
+}
+
 function kirby(x, y, s) {
   noStroke();
   push();
@@ -227,6 +211,41 @@ function kirby(x, y, s) {
   pop();
 }
 
+function createParticle() {
+  const x = Math.random() * width;
+  const y = -particleSize; // Set initial y position to a negative value
+  const v = 0.5 + Math.random() * 3.5; // Adjust the velocity range
+  const shape = Math.random() < 0.5 ? "square" : "circle";
+  const color = shape === "square" ? "green" : "red";
+  return { x: x, y: y, velocity: v, shape: shape, color: color };
+}
+
+function drawParticle(particle) {
+  push();
+  translate(particle.x, particle.y);
+  noStroke();
+
+  if (particle.shape === "square") {
+    fill(190, 255, 180);
+    rectMode(CENTER);
+    rect(0, 0, particleSize, particleSize);
+  } else if (particle.shape === "circle") {
+    fill(240, 100, 120);
+    ellipse(0, 0, particleSize, particleSize);
+  }
+
+  pop();
+}
+
+function updateParticle(particle) {
+  particle.y = particle.y + particle.velocity;
+
+  // Wrap the particle when it reaches the bottom of the canvas
+  if (particle.y > height + particleSize) {
+    particle.y = -particleSize;
+  }
+}
+
 function keyPressed() {
   if (keyCode === 37) {
     moveLeft = true;
@@ -251,75 +270,38 @@ function moveKirby() {
   }
 }
 
-function checkCollision(particle) {
-  const d = dist(x, y, particle.x, particle.y);
-
-  // Check for collision based on the shapes
-  if (particle.shape === "square") {
-    if (
-      x + 110 * s < particle.x + particleSize / 2 &&
-      x + 170 * s > particle.x - particleSize / 2 &&
-      y + 240 * s < particle.y + particleSize / 2 &&
-      y + 280 * s > particle.y - particleSize / 2
-    ) {
-      return true;
-    }
-  } else if (particle.shape === "circle") {
-    if (d < (particleSize / 2) * s) {
-      return true;
-    }
+function keyPressed() {
+  if (keyCode === 37) {
+    moveLeft = true;
+  } else if (keyCode === 39) {
+    moveRight = true;
   }
-
-  return false;
 }
 
-function updateScore() {
-  score += 1;
+function keyReleased() {
+  if (keyCode === 37) {
+    moveLeft = false;
+  } else if (keyCode === 39) {
+    moveRight = false;
+  }
 }
 
-function drawScore() {
-  fill(0);
-  textSize(20);
-  text("Score: " + score, 10, 30);
+function moveKirby() {
+  if (moveLeft) {
+    x -= 5;
+  } else if (moveRight) {
+    x += 5;
+  }
 }
 
 function draw() {
-  background(253, 212, 238);
-  // Move Kirby based on the key inputs
+  level1();
+  kirby(x + 540, y + 295, s * 0.3);
+  duck(x - 300, y + 150, s * 0.9);
   moveKirby();
-  kirby(x + 500, y + 295, s * 0.3);
 
-  for (let i = 0; i < particles.length; i++) {
-    const particle = particles[i];
-
-    // Check collision with Kirby
-    if (checkCollision(particle)) {
-      if (particle.color === "green") {
-        score += 1; // Increase the score by 1
-      } else if (particle.color === "red") {
-        score = 0; // Game over, reset the score to 0
-        particles = [];
-        setup(); // Reset the game
-      }
-
-      // Remove the collided particle and create a new one
-      particles.splice(i, 1);
-      particles.push(createParticle());
-
-      // Decrement the loop counter to account for the removed particle
-      i--;
-    }
-
-    // Draw and update the particle
+  for (let particle of particles) {
     drawParticle(particle);
     updateParticle(particle);
   }
-
-  // Display the score
-  textSize(24);
-  fill(255);
-  text("Score: " + score, 10, 30);
-
-  // Update Kirby's position
-  moveKirby();
 }
