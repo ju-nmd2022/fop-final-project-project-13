e3e39d9dca9c1c871558ed3912a8164b573f4f8c
miastu <mia.studenac@gmail.com>
2023-05-22 21:11:27 +0200

    update

--- game structure/game1.js
+++ game structure/game1.js
@@ -6,21 +6,42 @@ var character = {
   y: canvas.height - 30,
   width: 20,
   height: 20,
-  speed: 5,
+  speed: 3.5,
 };
 var obstacles = [];
 var score = 0;
+var gameOver = false;
+var obstacleSpeed = 1; // Adjust the initial speed of falling squares
+var obstacleInterval = 800; // Adjust the interval between the creation of new squares
+var elapsedTime = 0; // Track the elapsed time
 
-// Event listener for character movement
-document.addEventListener("keydown", moveCharacter);
+// Arrow key flags
+var leftArrowDown = false;
+var rightArrowDown = false;
 
-function moveCharacter(event) {
+// Event listeners for character movement
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
+
+function handleKeyDown(event) {
   if (event.key === "ArrowLeft") {
     // Left arrow key
     character.x -= character.speed;
+    leftArrowDown = true;
   } else if (event.key === "ArrowRight") {
     // Right arrow key
     character.x += character.speed;
+    rightArrowDown = true;
+  }
+}
+
+function handleKeyUp(event) {
+  if (event.key === "ArrowLeft") {
+    // Left arrow key
+    leftArrowDown = false;
+  } else if (event.key === "ArrowRight") {
+    // Right arrow key
+    rightArrowDown = false;
   }
 }
 
@@ -41,46 +62,69 @@ function update() {
   // Clear the canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw the character
-  context.fillStyle = "blue";
-  context.fillRect(character.x, character.y, character.width, character.height);
+  if (!gameOver) {
+    // Move the character
+    if (leftArrowDown) {
+      character.x -= character.speed;
+    } else if (rightArrowDown) {
+      character.x += character.speed;
+    }
 
-  // Draw the obstacles
-  for (var i = 0; i < obstacles.length; i++) {
-    var obstacle = obstacles[i];
-    context.fillStyle = obstacle.color;
-    context.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    // Draw the character
+    context.fillStyle = "blue";
+    context.fillRect(
+      character.x,
+      character.y,
+      character.width,
+      character.height
+    );
 
-    // Move the obstacles
-    obstacle.y += 2;
+    // Draw the obstacles
+    for (var i = 0; i < obstacles.length; i++) {
+      var obstacle = obstacles[i];
+      context.fillStyle = obstacle.color;
+      context.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
 
-    // Check for collision with character
-    if (
-      obstacle.y + obstacle.height >= character.y &&
-      obstacle.y <= character.y + character.height &&
-      obstacle.x + obstacle.width >= character.x &&
-      obstacle.x <= character.x + character.width
-    ) {
-      if (obstacle.color === "green") {
-        score++;
-      } else if (obstacle.color === "red") {
-        score--;
+      // Move the obstacles
+      obstacle.y += obstacleSpeed;
+
+      // Check for collision with character
+      if (
+        obstacle.y + obstacle.height >= character.y &&
+        obstacle.y <= character.y + character.height &&
+        obstacle.x + obstacle.width >= character.x &&
+        obstacle.x <= character.x + character.width
+      ) {
+        if (obstacle.color === "green") {
+          score++;
+        } else if (obstacle.color === "red") {
+          gameOver = true;
+        }
+        obstacles.splice(i, 1);
+        i--;
       }
-      obstacles.splice(i, 1);
-      i--;
+
+      // Remove obstacles that have gone off the screen or reached the bottom
+      if (
+        obstacle.y > canvas.height ||
+        (obstacle.color === "green" &&
+          obstacle.y + obstacle.height >= canvas.height)
+      ) {
+        obstacles.splice(i, 1);
+        i--;
+      }
     }
 
-    // Remove obstacles that have gone off the screen
-    if (obstacle.y > canvas.height) {
-      obstacles.splice(i, 1);
-      i--;
-    }
+    // Display the score
+    context.fillStyle = "black";
+    context.font = "20px Arial";
+    context.fillText("Score: " + score, 10, 20);
+  } else {
+    // Display game over message
+    context.fillStyle = "red";
+    context.font = "40px Arial";
+    context.fillText("Game Over", canvas.width);
   }
-
-  // Display the score
-  context.fillStyle = "black";
-  context.font = "20px Arial";
-  context.fillText("Score: " + score, 10, 20);
 
   // Request the next frame
   requestAnimationFrame(update);
